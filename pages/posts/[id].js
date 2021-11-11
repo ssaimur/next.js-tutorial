21e4f829d04e04adc333aaa4349467c415781269
Saimur Rahman <saimur715@gmail.com>
2021-11-11 07:15:20 +0600

    implemented basic dynamic route and api route

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,25 @@
+import Head from 'next/head';
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import Date from '../../components/dates';
+import utilStyles from '../../styles/utils.module.css';
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
 
 export async function getStaticPaths() {
   const paths = getAllPostIds();
@@ -10,24 +30,10 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
     },
   };
-}
-
-export default function Post({ postData }) {
-  return (
-    <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-    </Layout>
-  );
 }
